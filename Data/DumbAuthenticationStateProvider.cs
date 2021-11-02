8ba49925f2a512f7947acb634ae945c10b523821
Vlastimil Juračka <jurackavlasta@gmail.com>
2021-11-02 20:25:54 +0100

    Test

---
+++ Data/DumbAuthenticationStateProvider.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Components.Authorization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace WebApplication6.Data
+{
+    public class DumbAuthenticationStateProvider : AuthenticationStateProvider
+    {
+        public static ClaimsPrincipal User
+            => new ClaimsPrincipal(new ClaimsIdentity(UserClaims, "Dumb Auth Type"));
+
+        public static Claim[] UserClaims
+            => new[]{
+                    new Claim(ClaimTypes.Sid, "024672e0-250a-46fc-bd35-1902974cf9e1"),
+                    new Claim(ClaimTypes.Name, "Normal User"),
+                    new Claim(ClaimTypes.NameIdentifier, "Normal User"),
+                    new Claim(ClaimTypes.Email, "user@user,com"),
+                    new Claim(ClaimTypes.Role, "User")
+            };
+        public static ClaimsPrincipal Visitor
+            => new ClaimsPrincipal(new ClaimsIdentity(VisitorClaims, "Dumb Auth Type"));
+
+        public static Claim[] VisitorClaims
+            => new[]{
+                    new Claim(ClaimTypes.Sid, "324672e0-250a-46fc-bd35-1902974cf9e1"),
+                    new Claim(ClaimTypes.Name, "Visitor"),
+                    new Claim(ClaimTypes.NameIdentifier, "Normal Visitor"),
+                    new Claim(ClaimTypes.Email, "visitor@user,com"),
+                    new Claim(ClaimTypes.Role, "Visitor")
+            };
+
+        bool _switch;
+
+        public override Task<AuthenticationState> GetAuthenticationStateAsync()
+            => Task.FromResult(_switch
+                ? new AuthenticationState(User)
+                : new AuthenticationState(Visitor)
+                );
+
+        public Task<AuthenticationState> ChangeIdentity()
+        {
+            _switch = !_switch;
+            var task = this.GetAuthenticationStateAsync();
+            this.NotifyAuthenticationStateChanged(task);
+            return task;
+        }
+
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,71 @@
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication6.Data;
+
+namespace WebApplication6
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddAuthentication(
+                CookieAuthenticationDefaults.AuthenticationScheme)
+                .AddCookie();
+            services.AddAuthorization();
+
+            services.AddScoped<AuthenticationStateProvider, DumbAuthenticationStateProvider>();
+            //services.AddScoped<AuthenticationStateProvider>(provider => provider.GetRequiredService<DumbAuthenticationStateProvider>());
+            services.AddAuthorizationCore();
+
+            services.AddRazorPages();
+            services.AddServerSideBlazor();
+            services.AddSingleton<WeatherForecastService>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapBlazorHub();
+                endpoints.MapFallbackToPage("/_Host");
+            });
+        }
+    }
+}
